e9e81cf867a5e3260daf438eb542f585da10d3c0
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2022-06-30 20:50:08 +0300

    setting up the project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: false, //чтобы убрать ошибки сторонних старых библиотек
+	poweredByHeader: false, //для безопасности убираем информацию, что сайт сделан на Next.js
+	optimizeFonts: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+		APP_SERVER_URL: process.env.REACT_SERVER_URL,
+	},
+	//чтобы апи открывался не внутри некста на локалхост  3000
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `http://localhost:4200/api/:path*`,
+			},
+			{
+				source: '/uploads/:path*',
+				destination: `http://localhost:4200/uploads/:path*`,
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
