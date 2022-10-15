8d65bced31f166f9b4cd38c47ee9bb8abcb0a92a
Popenkov Anton <popenkovaa@yandex.ru>
2022-10-15 19:42:51 +0300

    search added

---
+++ app/components/layout/Sidebar/Search/SearchList/SearchList.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { FC } from 'react';
+
+import { getMovieUrl } from '@/configs/url.config';
+
+import { IWidgetMovie } from '../../MoviesContainer/movie.types';
+
+import styles from './SearchList.module.scss';
+
+const SearchList: FC<{ movies: IWidgetMovie[] }> = ({ movies }) => {
+	return (
+		<div className={styles.list}>
+			{movies.length ? (
+				movies.map((movie) => (
+					<Link key={movie._id} href={getMovieUrl(movie.slug)}>
+						<a>
+							<Image
+								src={movie.poster || ''}
+								width={50}
+								height={50}
+								objectFit="cover"
+								objectPosition="top"
+								alt={movie.title}
+								draggable={false}
+							/>
+							<span>{movie.title}</span>
+						</a>
+					</Link>
+				))
+			) : (
+				<div className="text-white text-center my-4">Movies not found!</div>
+			)}
+		</div>
+	);
+};
+
+export default SearchList;
