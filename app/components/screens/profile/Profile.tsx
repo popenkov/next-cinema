91064a3968784e3f128867de35ed4810d337f261
Popenkov Anton <popenkovaa@yandex.ru>
2022-11-02 17:26:14 +0300

    profile edit added

--- app/components/screens/profile/Profile.tsx
+++ app/components/screens/profile/Profile.tsx
@@ -1,52 +1,29 @@
-import { IProfileInput } from './profile.interface'
-import { FC } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { useMutation, useQuery } from 'react-query'
-import { toastr } from 'react-redux-toastr'
+import { FC } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useMutation, useQuery } from 'react-query';
+import { toastr } from 'react-redux-toastr';
 
-import AuthFields from '@/components/shared/user/AuthFields'
-import Button from '@/components/ui/form-elements/Button'
-import Heading from '@/components/ui/heading/Heading'
-import SkeletonLoader from '@/components/ui/skeleton-loader/SkeletonLoader'
+import AuthFields from '@/components/shared/user/AuthFields';
+import Button from '@/components/ui/form-elements/Button';
+import Heading from '@/components/ui/heading/Heading';
+import SkeletonLoader from '@/components/ui/skeleton-loader/SkeletonLoader';
 
-import { UserService } from '@/services/user/user.service'
+import { UserService } from '@/services/user/user.service';
 
-import { toastError } from '@/utils/api/withToastrErrorRedux'
-import { Meta } from '@/utils/meta'
+import { toastError } from '@/utils/api/withToastrErrorRedux';
+import { Meta } from '@/utils/meta';
 
-import styles from './Profile.module.scss'
+import styles from './Profile.module.scss';
+import { IProfileInput } from './profile.interface';
+import { useProfile } from './useProfile';
 
 const Profile: FC = () => {
 	const { handleSubmit, register, formState, setValue } =
 		useForm<IProfileInput>({
 			mode: 'onChange',
-		})
+		});
 
-	const { isLoading } = useQuery('profile', () => UserService.getProfile(), {
-		onSuccess({ data }) {
-			setValue('email', data.email)
-		},
-		onError(error) {
-			toastError(error, 'Get profile')
-		},
-	})
-
-	const { mutateAsync } = useMutation(
-		'update profile',
-		(data: IProfileInput) => UserService.updateProfile(data),
-		{
-			onError(error) {
-				toastError(error, 'Update profile')
-			},
-			onSuccess() {
-				toastr.success('Update profile', 'update was successful')
-			},
-		}
-	)
-
-	const onSubmit: SubmitHandler<IProfileInput> = async (data) => {
-		await mutateAsync(data)
-	}
+	const { onSubmit, isLoading } = useProfile(setValue);
 
 	return (
 		<Meta title="Profile">
@@ -65,7 +42,7 @@ const Profile: FC = () => {
 				<Button>Update</Button>
 			</form>
 		</Meta>
-	)
-}
+	);
+};
 
-export default Profile
+export default Profile;
